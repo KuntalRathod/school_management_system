3e9645437c85a7a84b2b3d76aff3eff90769c172
KuntalRathod <nature.kr70@gmail.com>
2025-02-28 12:16:33 +0530

    Refactor SQL query in getAllStudentClasses method; enhance readability and structure

--- app/models/studentClassModel.js
+++ app/models/studentClassModel.js
@@ -3,12 +3,15 @@ import db from "../../config/db.js"
 class StudentClassModel {
   static async getAllStudentClasses() {
     const query = `
-      SELECT sc.id, u.name AS student_name, c.class_name, s.subject_name 
-      FROM student_classes sc
-      JOIN users u ON sc.student_id = u.id
-      JOIN classes c ON sc.class_id = c.id
-      JOIN subjects s ON sc.subject_id = s.id
-      WHERE u.usertype='student';
+                   select sc.id , u.name as student_name , c.class_name , s.subject_name
+                            from student_classes sc
+                   inner join users u
+                            on sc.student_id = u.id
+                   inner join classes c
+                            on sc.class_id = c.id
+                   inner join subjects s
+                            on sc.subject_id = s.id
+                   where u.usertype = "student";
     `
     try {
       const [rows] = await db.execute(query)
